9ab8ac2fc2b325550606c656dfd2722f32fa2d52
look-luc <denardiluc@gmail.com>
2026-09-10 17:14:16 -0600

    trying to see some columns and rows

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from dataframe import patent_DataFrame
 from rdd import patent_RDD
 def main():
     df = patent_DataFrame()
-    print(df)
+    df.show(5)
+    # print(df)
 
 if __name__ == "__main__":
     main()
