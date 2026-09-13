7addecd64762823b1cae599a8b8540bb397b4411
look-luc <denardiluc@gmail.com>
2026-09-13 13:55:47 -0600

    working on rdd

--- rdd.py
+++ rdd.py
@@ -1,14 +1,23 @@
 from pyspark import SparkContext, SparkConf
+from pyspark import RDD
 
+def parse(csv:RDD[str]):
+    header = csv.first()
+    return (
+        csv.filter(lambda line: line != header)
+        .map(lambda line: line.split(","))
+        .map(lambda parts: (int(parts[0]), parts[1:]))
+    )
 
 def patent_RDD():
     conf = SparkConf().setAppName("Lab4-rdd").setMaster("local[*]")
     sc = SparkContext(conf=conf)
 
     rddCitations = sc.textFile("cite75_99.txt.gz")
+    rdd_citations_sample = rddCitations.sample(False, 0.1).cache()
+    
     rddPatents = sc.textFile("apat63_99.txt.gz")
-    return rddCitations, rddPatents
+    rdd_patents_sample = rddPatents.sample(False, 0.1).cache()
 
-if __name__ == "__main__":
-    citations, patents = RDD()
-    print(f"citation type: {type(citations)}\npatent type: {type(patents)}")
+    citations = parse(rdd_citations_sample).cache()
+    patents = parse(rdd_patents_sample).cache()
