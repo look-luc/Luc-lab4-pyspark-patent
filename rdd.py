b93a0d04cc5335610007bd72f6ffb73fc1032482
look-luc <denardiluc@gmail.com>
2026-09-13 14:06:09 -0600

    adding back header

--- rdd.py
+++ rdd.py
@@ -1,13 +1,18 @@
 from pyspark import SparkContext, SparkConf
 from pyspark import RDD
 
-def parse(csv:RDD[str]):
-    header = csv.first()
-    return (
-        csv.filter(lambda line: line != header)
-        .map(lambda line: line.split(","))
-        .map(lambda parts: (int(parts[0]), parts[1:]))
+def parse(csv:RDD[str], sc:SparkContext):
+    header_string = csv.first()
+    data_rows = csv.filter(lambda line: line != header_string)
+
+    processed_data = data_rows.map(lambda line: line.split(",")).map(
+        lambda parts: (int(parts[0]), parts[1:])
     )
+
+    csv_data_rows = processed_data.map(lambda kv: f"{kv[0]},{','.join(kv[1])}")
+
+    header_rdd = sc.parallelize([header_string])
+    return header_rdd.union(csv_data_rows)
 
 def patent_RDD():
     conf = SparkConf().setAppName("Lab4-rdd").setMaster("local[*]")
@@ -19,7 +24,7 @@ def patent_RDD():
     rddPatents = sc.textFile("apat63_99.txt.gz")
     rdd_patents_sample = rddPatents.sample(False, 0.1).cache()
 
-    citations = parse(rdd_citations_sample).cache().take(5)
-    patents = parse(rdd_patents_sample).cache().take(5)
+    citations = parse(csv=rdd_citations_sample, sc=sc).cache().take(5)
+    patents = parse(csv=rdd_patents_sample, sc=sc).cache().take(5)
 
     return citations, patents
