5adcbd3c90bc51e65e23a64c9d61aedc92ef1aa2
look-luc <denardiluc@gmail.com>
2026-09-13 15:16:29 -0600

    added back headers including the co state

--- rdd.py
+++ rdd.py
@@ -8,53 +8,55 @@ def patent_RDD(
     co_state='"POSTATE"',
     takes: int | None = None,
 ):
-  conf = SparkConf().setAppName("Lab4-rdd").setMaster("local[*]")
-  sc = SparkContext.getOrCreate(conf=conf)
+    conf = SparkConf().setAppName("Lab4-rdd").setMaster("local[*]")
+    sc = SparkContext.getOrCreate(conf=conf)
 
-  rdd_citations = sc.textFile("cite75_99.txt.gz")
-  rdd_patents = sc.textFile("apat63_99.txt.gz")
+    rddCitations = sc.textFile("cite75_99.txt.gz")
+    rddPatents = sc.textFile("apat63_99.txt.gz")
 
-  citations_header = rdd_citations.first().split(",")
-  patents_header = rdd_patents.first().split(",")
+    citations_header = rddCitations.first().split(",")
+    patents_header = rddPatents.first().split(",")
 
-  citing_idx = citations_header.index(citing_name)
-  cited_idx = citations_header.index(cited_name)
-  patent_idx = patents_header.index(patent_name)
-  state_idx = patents_header.index(co_state)
+    citing_idx = citations_header.index(citing_name)
+    cited_idx = citations_header.index(cited_name)
+    patent_idx = patents_header.index(patent_name)
+    state_idx = patents_header.index(co_state)
 
-  citations_clean = rdd_citations.filter(
-      lambda l: not l.startswith(citing_name)
-  )
-  patents_clean = rdd_patents.filter(lambda l: not l.startswith(patent_name))
+    citations_clean = rddCitations.filter(lambda l: not l.startswith(citing_name))
+    patents_clean = rddPatents.filter(lambda l: not l.startswith(patent_name))
 
-  citations_by_cited = citations_clean.map(lambda l: l.split(",")).map(
-      lambda p: (p[cited_idx], p[citing_idx])
-  )
-  patents_state = patents_clean.map(lambda l: l.split(",")).map(
-      lambda p: (p[patent_idx], p[state_idx].replace('"', ""))
-  )
+    citations_by_cited = citations_clean.map(lambda l: l.split(",")).map(
+        lambda p: (p[cited_idx], p[citing_idx])
+    )
+    patents_state = patents_clean.map(lambda l: l.split(",")).map(
+        lambda p: (p[patent_idx], p[state_idx].replace('"', ""))
+    )
 
-  cited_joined = citations_by_cited.join(patents_state)
+    cited_joined = citations_by_cited.join(patents_state)
 
-  citing_keyed = cited_joined.map(lambda x: (x[1][0], x[1][1]))
+    citing_keyed = cited_joined.map(lambda x: (x[1][0], x[1][1]))
 
-  citing_joined = citing_keyed.join(patents_state)
+    citing_joined = citing_keyed.join(patents_state)
 
-  filtered_matches = citing_joined.filter(
-      lambda x: x[1][0] != "" and x[1][1] != "" and x[1][0] == x[1][1]
-  )
+    filtered_matches = citing_joined.filter(
+        lambda x: x[1][0] != "" and x[1][1] != "" and x[1][0] == x[1][1]
+    )
 
-  same_state_counts = filtered_matches.map(lambda x: (x[0], 1)).reduceByKey(
-      lambda a, b: a + b
-  )
+    same_state_counts = filtered_matches.map(lambda x: (x[0], 1)).reduceByKey(
+        lambda a, b: a + b
+    )
 
-  patents_by_id = patents_clean.map(lambda l: (l.split(",")[patent_idx], l))
-  result = patents_by_id.leftOuterJoin(same_state_counts)
+    patents_by_id = patents_clean.map(lambda l: (l.split(",")[patent_idx], l))
+    result = patents_by_id.leftOuterJoin(same_state_counts)
 
-  final_rdd = result.map(
-      lambda x: (x[0], x[1][0], x[1][1] if x[1][1] is not None else 0)
-  ).sortBy(lambda x: x[2], ascending=False)
+    header_string = rddPatents.first()
+    header_rdd = sc.parallelize([header_string, "CO_STATE"])
 
-  if takes is not None:
-    return final_rdd.take(takes)
-  return final_rdd
+    final_rdd = result.map(
+        lambda x: (x[0], x[1][0], x[1][1] if x[1][1] is not None else 0)
+    ).sortBy(lambda x: x[2], ascending=False)
+
+    header_rdd.union(final_rdd)
+    if takes is not None:
+        return header_rdd.take(takes)
+    return header_rdd
