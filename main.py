bc9c2ee076aac7d6890a467062f8888efaa86a4f
look-luc <denardiluc@gmail.com>
2026-09-13 14:01:00 -0600

    putting a print statement

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     # df = patent_DataFrame()
     # df.show()
     citation, patent = patent_RDD()
-
+    print(f"citations: {citation}\npatents: {patent}")
 
 if __name__ == "__main__":
     main()
